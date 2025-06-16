f24c4d1d941681149ba4a0f5344ffe3cca5fe44d
LWai@itiinc.co.jp <LWai@itiinc.co.jp>
2025-06-16 18:05:28 +0900

    My Resumes fix

--- src/components/containers/EducationTimeline.jsx
+++ src/components/containers/EducationTimeline.jsx
@@ -6,33 +6,49 @@ import { useQuery } from "react-query";
 import { getEducationBackground } from "../../fetchers";
 import { childrenAnimation } from "../../lib/motion";
 import { TimelineItem } from "../elements";
+import i18next from "i18next";
+import { useEffect, useState } from "react";
 
 const EducationTimeline = () => {
-	const { data } = useQuery("education-background", getEducationBackground);
+  const { data } = useQuery("education-background", getEducationBackground);
+  const [lng, setLng] = useState(i18next.language);
 
-	if (!data) return null;
+  useEffect(() => {
+    const handleLangChange = (lng) => setLng(lng);
+    i18next.on("languageChanged", handleLangChange);
+    return () => i18next.off("languageChanged", handleLangChange);
+  }, []);
 
-	return (
-		<div className="education-timeline">
-			<h4>
-				<RiBookLine className="mr-2 inline-block text-primary" />
-				Educational Qualification
-			</h4>
-			{data?.map((timeline, index) => (
-				<motion.div
-					initial="hidden"
-					whileInView="visible"
-					viewport={{ once: true }}
-					transition={{ duration: 0.4, delay: 0.2 * index }}
-					variants={childrenAnimation}
-					className="timeline-wrap"
-					key={timeline.id}
-				>
-					<TimelineItem timeline={timeline} />
-				</motion.div>
-			))}
-		</div>
-	);
+  if (!data) return null;
+
+  return (
+    <div className="education-timeline">
+      <h4>
+        <RiBookLine className="mr-2 inline-block text-primary" />
+        {lng === "jp" ? "学歴" : "Educational Qualification"}
+      </h4>
+      {data?.map((timeline, index) => (
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.4, delay: 0.2 * index }}
+          variants={childrenAnimation}
+          className="timeline-wrap"
+          key={timeline.id}
+        >
+          <TimelineItem
+            timeline={{
+              ...timeline,
+              title: lng === "jp" ? timeline.title_jp : timeline.title_en,
+              meta: lng === "jp" ? timeline.meta_jp : timeline.meta_en,
+              text: lng === "jp" ? timeline.text_jp : timeline.text_en,
+            }}
+          />
+        </motion.div>
+      ))}
+    </div>
+  );
 };
 
 export default EducationTimeline;
--- src/components/containers/JobTimeline.jsx
+++ src/components/containers/JobTimeline.jsx
@@ -4,33 +4,49 @@ import { useQuery } from "react-query";
 import { getJobExperience } from "../../fetchers";
 import { childrenAnimation } from "../../lib/motion";
 import { TimelineItem } from "../elements";
+import i18next from "i18next";
+import { useEffect, useState } from "react";
 
 const JobTimeline = () => {
-    const { data } = useQuery("job-experience", getJobExperience);
+  const { data } = useQuery("job-experience", getJobExperience);
+  const [lng, setLng] = useState(i18next.language);
 
-    if (!data) return null;
+  useEffect(() => {
+    const handleLangChange = (lng) => setLng(lng);
+    i18next.on("languageChanged", handleLangChange);
+    return () => i18next.off("languageChanged", handleLangChange);
+  }, []);
 
-    return (
-        <div className="job-experience">
-            <h4>
-                <RiBriefcaseLine className="mr-2 inline-block text-primary" />
-                Working Experience
-            </h4>
-            {data?.map((timeline, index) => (
-                <motion.div
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true }}
-                    transition={{ duration: 0.4, delay: 0.2 * index }}
-                    variants={childrenAnimation}
-                    className="timeline-wrap"
-                    key={timeline.id}
-                >
-                    <TimelineItem timeline={timeline} />
-                </motion.div>
-            ))}
-        </div>
-    );
+  if (!data) return null;
+
+  return (
+    <div className="job-experience">
+      <h4>
+        <RiBriefcaseLine className="mr-2 inline-block text-primary" />
+        {lng === "jp" ? "職務経歴" : "Working Experience"}
+      </h4>
+      {data?.map((timeline, index) => (
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 0.4, delay: 0.2 * index }}
+          variants={childrenAnimation}
+          className="timeline-wrap"
+          key={timeline.id}
+        >
+          <TimelineItem
+            timeline={{
+              ...timeline,
+              title: lng === "jp" ? timeline.title_jp : timeline.title_en,
+              meta: lng === "jp" ? timeline.meta_jp : timeline.meta_en,
+              text: lng === "jp" ? timeline.text_jp : timeline.text_en,
+            }}
+          />
+        </motion.div>
+      ))}
+    </div>
+  );
 };
 
 export default JobTimeline;
