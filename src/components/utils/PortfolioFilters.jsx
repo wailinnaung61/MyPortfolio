7869bc6fea869b7c3d4aa254d71f64cb66588cb7
LWai@itiinc.co.jp <LWai@itiinc.co.jp>
2025-06-20 17:43:11 +0900

    works fix

--- src/components/utils/PortfolioFilters.jsx
+++ src/components/utils/PortfolioFilters.jsx
@@ -2,33 +2,46 @@
 
 import { useQuery } from "react-query";
 import { getPortfolioFilters } from "../../fetchers";
+import { useEffect, useState } from "react";
+import i18next from "i18next";
 
 const PortfolioFilters = ({ currentFilter, filterHandler }) => {
-	const { data } = useQuery("portfolio-filters", getPortfolioFilters);
+  const { data } = useQuery("portfolio-filters", getPortfolioFilters);
+  const [lng, setLng] = useState(i18next.language);
 
-	if (!data) return null;
+  useEffect(() => {
+    const handleLangChange = (lng) => setLng(lng);
+    i18next.on("languageChanged", handleLangChange);
+    return () => i18next.off("languageChanged", handleLangChange);
+  }, []);
 
-	return (
-		<div className="portfolio-filters flex flex-wrap justify-center gap-4">
-			<button
-				className={`btn btn-small ${currentFilter === "" ? "" : "btn-transparent"}`}
-				onClick={() => filterHandler("")}
-			>
-				<span>All</span>
-			</button>
-			{data?.map((filter) => (
-				<button
-					className={`btn btn-small ${
-						currentFilter === filter.value ? "before:invisible" : "btn-transparent"
-					}`}
-					onClick={() => filterHandler(filter.value)}
-					key={filter.id}
-				>
-					<span>{filter.title}</span>
-				</button>
-			))}
-		</div>
-	);
+  if (!data) return null;
+
+  return (
+    <div className="portfolio-filters flex flex-wrap justify-center gap-4">
+      <button
+        className={`btn btn-small ${
+          currentFilter === "" ? "" : "btn-transparent"
+        }`}
+        onClick={() => filterHandler("")}
+      >
+        <span>{lng === "jp" ? "全て" : "All"}</span>
+      </button>
+      {data?.map((filter) => (
+        <button
+          className={`btn btn-small ${
+            currentFilter === filter.value
+              ? "before:invisible"
+              : "btn-transparent"
+          }`}
+          onClick={() => filterHandler(filter.value)}
+          key={filter.id}
+        >
+          <span>{lng === "jp" ? filter.title_jp : filter.title_en}</span>
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default PortfolioFilters;
